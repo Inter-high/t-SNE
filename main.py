7d502f54273bc9f948fe1de01c43f13f1d62206a
Inter-high <yumemonzo@gmail.com>
2024-12-16 11:49:55 +0000

    Update: main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def calc_high_dim_sim(x, x_i, x_j, sigma):
 P = np.zeros((3, 3))
 indices = [(0, 1), (0, 2), (1, 2)]
 for i, j in indices:
-    P[i, j] = calc_high_dim_sim(x, x[i], x[j], sigma)
+    P[i, j] = round(calc_high_dim_sim(x, x[i], x[j], sigma), 3)
     P[j, i] = P[i, j]  # 대칭 행렬로 가정
 print(P)
 
@@ -45,6 +45,47 @@ def calc_low_dim_sim(y, y_i, y_j):
 Q = np.zeros((3, 3))
 indices = [(0, 1), (0, 2), (1, 2)]
 for i, j in indices:
-    Q[i, j] = calc_low_dim_sim(y, y[i], y[j])
+    Q[i, j] = round(calc_low_dim_sim(y, y[i], y[j]), 3)
     Q[j, i] = Q[i, j]  # 대칭 행렬로 가정
 print(Q)
+
+
+def calc_cost(p, q):
+    indices = [(0, 1), (0, 2), (1, 2)]
+    cost = 0.0
+    for i, j in indices:
+        cost += p[i, j] * round(np.log(p[i, j] / q[i, j]), 3)
+    
+    return cost
+
+cost = calc_cost(P, Q)
+print(f'{cost}\n')
+
+
+def calc_gradient(p, q, y):
+    """
+    TODO: gradient_list 할당 수식 부분 디버깅
+    """
+    indices = [(0, 1), (0, 2), (1, 2)]
+    gradient_list = []
+    minus_list = []
+    weight_list = []
+    vector_diff_list = []
+    for i, j in indices:
+        minus = round(p[i, j] - q[i, j], 3)
+        weight = round((1 + calc_distance(y[i], y[j])) ** -1, 3)
+        vector_diff = y[i] - y[j]
+        minus_list.append(minus)
+        weight_list.append(weight)
+        vector_diff_list.append(vector_diff)
+
+    for i, j in indices:
+        gradient_list.append(4 * (
+            (minus_list[i] * np.dot(vector_diff_list[i], weight_list[i])) +
+            (minus_list[j] * np.dot(vector_diff_list[j], weight_list[j]))
+        ))
+
+    return [np.round(arr, 3) for arr in gradient_list]
+
+gradient = calc_gradient(P, Q, y)
+print(gradient)
