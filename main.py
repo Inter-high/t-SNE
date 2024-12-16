47adbe29bf77ced011dc1140996817627977c4a5
Inter-high <yumemonzo@gmail.com>
2024-12-16 17:10:54 +0000

    Update: main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-TODO: Sigma 계산하는 함수 작성
+TODO: epoch 증가 시, cost 값 증가하는 버그 디버깅
 """
 
 import itertools
@@ -22,6 +22,29 @@ class TSNE:
         distance = np.sum((i -j) ** 2)
 
         return distance
+
+    def calc_sigma(self, x, i_idx, j_idx, target_perplexity=2, sigma_min=1e-5, sigma_max=50.0):
+        while True:
+            sigma = (sigma_min + sigma_max) / 2
+            numerator = np.exp(-1 * ((self.calc_distance(x[i_idx], x[j_idx])) / (2 * sigma ** 2)))
+            denominator = 0.0
+            for i, x_k in enumerate(x):
+                if i != i_idx:
+                    denominator += np.exp(-1 * ((self.calc_distance(x[i_idx], x_k)) / (2 * sigma ** 2)))
+                
+            p_ji = numerator / denominator
+            h_pi = p_ji * np.log2(p_ji)
+            perplexity = 2 ** h_pi
+            
+            if perplexity > target_perplexity:
+                sigma_max = sigma
+            elif perplexity < target_perplexity:
+                sigma_min = sigma
+
+            if perplexity - target_perplexity < 1e-4:
+                break
+
+        return sigma
 
     def calc_high_dim_sim(self, x, x_i, x_j, sigma):
         numerator = np.exp(-1 * (self.calc_distance(x_i, x_j)) / (2 * sigma ** 2))
@@ -85,10 +108,16 @@ class TSNE:
 
     def train(self, x, epoch=100):
         for num in range(epoch):
+            indices = [(0, 1), (0, 2), (1, 2)]
+            sigma_list = []
+            for i, j in indices:
+                sigma = self.calc_sigma(x, i, j)
+                sigma_list.append(sigma)
+
             P = np.zeros((3, 3))
             indices = [(0, 1), (0, 2), (1, 2)]
-            for i, j in indices:
-                P[i, j] = round(self.calc_high_dim_sim(x, x[i], x[j], sigma), 3)
+            for idx, (i, j) in enumerate(indices):
+                P[i, j] = round(self.calc_high_dim_sim(x, x[i], x[j], sigma_list[idx]), 3)
                 P[j, i] = P[i, j]  # 대칭 행렬로 가정
             # print(P)
 
